fef6ed7784c872707b797e75abdc47a4b790d1ab
PranauR <ajaypranauer10@gmail.com>
2022-06-24 19:18:34 +0530

    #1: Changed header

--- examples/header_test/header_test.ino
+++ examples/header_test/header_test.ino
@@ -6,16 +6,10 @@ Function:
 	Test header file for MCCI_Catena_LTR329 library
 
 Copyright and License:
-	This file copyright (C) 2020 by
-
-		MCCI Corporation
-		3520 Krums Corners Road
-		Ithaca, NY  14850
-
 	See accompanying LICENSE file for copyright and license information.
 
 Author:
-	Pranau, MCCI Corporation    May 2022
+	Pranau, MCCI Corporation    June 2022
 
 */
 
